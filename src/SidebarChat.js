84e56cc34f5d8a2d06cfd90f76a9be807e9569dc
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-03-06 11:32:42 +0530

    Add files via upload

---
+++ src/SidebarChat.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import './SidebarChat.css'
+import { Link } from 'react-router-dom'
+import { Avatar } from '@material-ui/core'
+import db from './firebase'
+
+function SidebarChat({ id, name, addNewChat }) {
+
+    const [seed, setSeed] = useState('');
+    const [messages, setMessages] = useState('');
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
+    useEffect(() => {
+        if (id) {
+            db.collection('rooms')
+                .doc(id)
+                .collection('messages')
+                .orderBy('timestamp', 'desc')
+                .onSnapshot((snapshot) =>
+                    setMessages(snapshot.docs.map((doc) => doc.data()))
+                );
+            console.log("mes", messages)
+        }
+    }, [id]);
+
+
+    const createChat = () => {
+        const roomName = prompt("please enter name for chat");
+
+        if (roomName) {
+            //do database stuff....
+            db.collection("rooms").add({
+                name: roomName,
+            })
+        }
+    };
+
+    return !addNewChat ? (
+        <Link to={`/rooms/${id}`}>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>{messages[0]?.messages}</p>
+                </div>
+            </div>
+        </Link>
+    ) : (
+            <div onClick={createChat}
+                className="sidebarChat">
+                <h2>Add new Chat</h2>
+            </div>
+        )
+}
+
+export default SidebarChat
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    // here goes firebase config details !!!
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
